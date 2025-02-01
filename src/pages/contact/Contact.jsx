b4b6a519b837534e953437abc9b618460da4cd96
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-02-02 02:56:35 +0600

    versel error solve

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ContactBannerSection from './ContactBannerSection'
 import GoogleMap from './GoogleMap'
-import Appointment from '../home/appointment'
+import Appointment from '../home/Appointment'
 import News from '../../components/News'
 
 function Contact() {
