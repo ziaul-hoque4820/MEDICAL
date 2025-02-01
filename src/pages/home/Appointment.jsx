b6f17a1a47d1adaab1625e7e62481d40e2c689d3
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-02-01 23:06:36 +0600

    another half done

--- src/pages/home/Appointment.jsx
+++ src/pages/home/Appointment.jsx
@@ -1,10 +1,31 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    gender:yup.string().required(),
+    email: yup.string().required(),
+    phone:yup.number().positive().integer().required(),
+    doctor:yup.string(),
+    department:yup.string(),
+    message: yup.string()
+  })
+  .required()
 
 function Appointment() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({ resolver: yupResolver(schema),})
+    
+      const onSubmit = (data) => console.log(data)
     return (
         <section className="">
             <div
-                className="w-full lg:h-[500px] sm:h-[480px] p-9"
+                className="w-full p-9"
                 style={{
                     backgroundImage: `url(https://images.unsplash.com/photo-1581056771107-24ca5f033842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
                     backgroundSize: 'cover',
@@ -13,7 +34,7 @@ function Appointment() {
                 }}>
                 <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center gap-8 lg:gap-12">
                     {/* Text Section */}
-                    <div className="w-full lg:w-1/2 text-center lg:text-left lg:mt-12 hidden md:block lg:block md:hidden">
+                    <div className="w-full lg:w-1/2 text-center lg:text-left lg:mt-12 hidden md:block lg:block ">
                         <h2 className="text-4xl font-bold text-blue-600 mb-4">Book an Appointment</h2>
                         <p className="text-gray-200">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare.
@@ -24,9 +45,11 @@ function Appointment() {
 
                     {/* Form Section */}
                     <div className="w-full lg:w-1/2 bg-blue-900 p-6 rounded-lg shadow-lg lg:mt-0 lg:self-start">
-                        <form onSubmit={(e) => e.preventDefault()} className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-white">
-                            <input className="p-2 bg-blue-800 rounded placeholder-white" placeholder="Name" />
+                        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-white">
+                            <input {...register("name")} className="p-2 bg-blue-800 rounded placeholder-white" placeholder="Name" />
+                            {errors.name && <p className="text-red-400">{errors.name.message}</p> }
                             <select
+                                {...register("gender")}
                                 id="gender"
                                 className="bg-blue-800 border border-blue-500 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                 <option selected>Choose a Gender</option>
@@ -34,15 +57,20 @@ function Appointment() {
                                 <option value="Female">Female</option>
                                 <option value="Non-binary">Non-binary</option>
                             </select>
+                            {errors.gender && <p className="text-red-400">{errors.gender.message}</p> }
                             <input
+                                {...register("email")}
                                 type="email"
                                 className="p-2 bg-blue-800 rounded text-white placeholder-white"
                                 placeholder="Email"
                             />
-                            <input className="p-2 bg-blue-800 rounded placeholder-white" placeholder="Phone" />
-                            <input type="date" className="p-2 bg-blue-800 rounded" />
-                            <input type="time" className="p-2 bg-blue-800 rounded" />
+                            {errors.email && <span className="text-red-400">{errors.email.message}</span> }
+                            <input {...register("phone")} className="p-2 bg-blue-800 rounded placeholder-white" placeholder="Phone" />
+                            {errors.phone && <p className="text-red-400">{errors.phone.message}</p> }
+                            <input {...register("date")} type="date" className="p-2 bg-blue-800 rounded" />
+                            <input {...register("time")} type="time" className="p-2 bg-blue-800 rounded" />
                             <select
+                                {...register("doctor")}
                                 id="doctor"
                                 className="bg-blue-800 border border-blue-500 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                 <option selected>Choose a Doctor</option>
@@ -52,7 +80,9 @@ function Appointment() {
                                 <option value="Dr. Adam Brown">Dr. Adam Brown</option>
                                 <option value="Dr. Emily White">Dr. Emily White</option>
                             </select>
+                            {errors.doctor && <p className="text-red-400">{errors.doctor.message}</p> }
                             <select
+                                {...register("department")}
                                 id="department"
                                 className="bg-blue-800 border border-blue-500 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                 <option selected>Choose a Department</option>
@@ -62,9 +92,12 @@ function Appointment() {
                                 <option value="Dermatology">Dermatology</option>
                                 <option value="Pediatrics">Pediatrics</option>
                             </select>
+                            {errors.department && <p className="text-red-400">{errors.department.message}</p> }
                             <textarea
+                                {...register("message")}
                                 className="p-2 bg-blue-800 rounded col-span-1 sm:col-span-2 placeholder-white"
                                 placeholder="Message"></textarea>
+                                {errors.message && <p className="text-red-400">{errors.message.message}</p> }
                             <button
                                 type="submit"
                                 className="p-2 bg-blue-600 rounded col-span-1 sm:col-span-2 hover:bg-blue-500">
