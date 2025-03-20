bf9d343698d408a4d3c8b3299367c9094a017b2a
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-03-20 21:30:23 +0600

    change appointment section

--- src/pages/home/Appointment.jsx
+++ src/pages/home/Appointment.jsx
@@ -29,7 +29,7 @@ function Appointment() {
     return (
         <section className="">
             <div
-                className="w-full p-9"
+                className="w-full p-8"
                 style={{
                     backgroundImage: `url(https://images.unsplash.com/photo-1581056771107-24ca5f033842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
                     backgroundSize: 'cover',
